f06c1d302207b84aa210ee9b89d12406834311b7
Jacob Lindelof <jlindelof@me.com>
2023-02-19 09:43:00 -0500

    testing

---
+++ app.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="min-h-screen bg-purple-600">
+  </div>
+</template>
---
+++ assets/resume.ts
@@ -0,0 +1,149 @@
+export default {
+  firstName: "Jacob",
+  lastName: "Lindelof",
+  title: "Software Engineer",
+  email: "jacob@lindelof.dev",
+  phoneNumber: "+1 (727) 403-9612",
+  city: "Safety Harbor",
+  state: "FL",
+  summary: "Full stack software engineer with over 5 years of experience developing production ready applications in multiple programming languages and frameworks. As well as more than a decade of experience working with both Unix and Windows servers. My varied experience and aptitude for picking up new skills quickly allow me to jump between projects and environments with ease.",
+  languages: ["Python", "JavaScript", "TypeScript", "C#", "SQL"],
+  frameworks: [
+    "Django",
+    "Django Rest Framework",
+    "Flask",
+    "FastAPI",
+    "Vue",
+    "Electron",
+    "ASP.NET",
+    "Entity Framework",
+    "Windows Presentation Foundation (WPF)",
+    "Universal Windows Platform (UWP)",
+  ],
+  technologies: [
+    "Docker",
+    "Amazon Web Services (AWS)",
+    "VMWare vSphere",
+    "Unix Server",
+    "Windows Server",
+    "PostreSQL",
+    "Microsoft SQL Server",
+    "Stripe",
+  ],
+  workExperience: [
+    {
+      company: "Blue Streak Docs, LLC",
+      location: "Clearwater, FL",
+      type: "Professional",
+      positions: [
+        {
+          title: "VP of Information Technology",
+          startDate: "June 2018",
+          endDate: "Present",
+          highlights: [
+            "Helped the company transition into a modern technology focused company that uses software and solutions to provide a better product to our clients, with reduced costs and increased productivity",
+            "Utilized machine learning workflows for automating the interaction with real estate documents using AWS (Amazon Web Services) Textract and Comprehend.",
+            "Implemented multiple microservices (Python, C#) to support integration with our workflow software, 3rd party client systems, and other services.",
+            "Developed an inhouse desktop PDF viewer (C#/WPF) that integrated into our 3rd party workflow software *Providing* enhanced automation and efficiency when working with PDFs.",
+            "Created a full stack company intranet (Django, Vue) with in-depth reporting and business intelligence functionality, custom integrations with our workflow software, and tools to automate manual business processes.",
+            "Launched a full e-commerce website for individuals and smaller companies to order our services outside of our traditional workflow (Django, Vue, Stripe).",
+            "Migrated legacy systems to a fully virtualized highly available VMWare environment allowing for enhanced redundancy and reduced maintenance costs.",
+          ],
+        },
+        {
+          title: "IT Manager",
+          startDate: "September 2013",
+          endDate: "June 2018",
+          highlights: [
+            "Management of the company's IT infrastructure, including servers, software, and networking equipment.",
+            "Developed a basic business intelligence dashboard for monitoring of company production statistics.",
+          ],
+        },
+      ],
+    },
+    {
+      company: "Tampa Esports",
+      location: "Largo, FL",
+      type: "Independent",
+      positions: [
+        {
+          title:
+            "Event Coordinator, Systems Administrator, Tournament Organizer",
+          startDate: "July 2013",
+          endDate: "Present",
+          highlights: [
+            "Participated in event planning for multiple 200+ attendee esports and LAN events a year.",
+            "Handed setup and maintenance of our temporary network and server infrastructure.",
+            "Ran our headlining 16+ team Counter Strike: Global Offensive tournament.",
+          ],
+        },
+      ],
+    },
+    {
+      company: "Esports Arena",
+      location: "Santa Ana, CA",
+      type: "Independent",
+      positions: [
+        {
+          title: "Game Server Administrator",
+          startDate: "June 2016",
+          endDate: "February 2017",
+          highlights: [
+            "Performed onsite setup of multiple Counter Strike game servers to support semi regular in person tournaments.",
+            "Managed the tournaments both onsite and remotely until they were able to hire someone for me to train in the management of the servers.",
+          ],
+        },
+      ],
+    },
+    {
+      company: "Dreamhack",
+      location: "Austin, TX",
+      type: "Independent",
+      positions: [
+        {
+          title: "Network Admin",
+          startDate: "May 2016",
+          endDate: null,
+          highlights: [
+            "Pre event setup and post event teardown of the temporary 1000+ user network infrastructure.",
+            "Management of a network with rapidly fluctuating usage that needed real time traffic shaping to meet the level of quality that our attendees expected, and to support the multiple high profile esports tournaments throughout the event.",
+          ],
+        },
+      ],
+    },
+    {
+      company: "Pinellas Computers",
+      location: "Largo, FL",
+      type: "Professional",
+      positions: [
+        {
+          title: "Computer Technician",
+          startDate: "August 2012",
+          endDate: "September 2013",
+          highlights: [
+            "Aided in the development and implementation of a ticking system for customer service tracking.",
+            "Helped grow our small business division by providing our customers with server setup, email migrations, backup solutions, and more.",
+            "Automated various in-house diagnostic tasks to simplify our workflow and increase productivity.",
+          ],
+        },
+      ],
+    },
+    {
+      company: "Peter Wittwer",
+      location: "Largo, FL",
+      type: "Professional",
+      positions: [
+        {
+          title: "Junior Systems Administrator",
+          startDate: "June 2010",
+          endDate: "August 2012",
+          highlights: [
+            "Managed onsite network and server infrastructure including Active Directory, SQL, Exchange, and Web servers.",
+            "Zero downtime migration from locally hosted Exchange Server to Microsoft Online Services.",
+            "Executed a physical office relocation and simultaneous upgrade from Windows XP to Windows 7 with little to no negative impact to our employees.",
+          ],
+        },
+      ],
+    },
+  ],
+};
